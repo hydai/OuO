b589ee0f5d137a0a446c8c5d323512524c273045
henryyang42 <henryyang42@gmail.com>
2015-08-22 00:45:46 +0800

    add graoh

--- schema/admin.py
+++ schema/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from schema.models import Field, Template
+from schema.models import Field, Mapping, Template, Graph
 
 admin.site.register(Field)
+admin.site.register(Mapping)
 admin.site.register(Template)
+admin.site.register(Graph)
--- schema/models.py
+++ schema/models.py
@@ -14,16 +14,38 @@ class Field(models.Model):
         return '%d - %s(%s)' % (self.id, self.fname, self.ftype)
 
 
+class Mapping(models.Model):
+    field = models.ForeignKey(Field)
+    onto = models.CharField(max_length=50, default='')
+
+    def __unicode__(self):
+        return '%d - [%s -> %s]' % (self.id, self.field.fname, self.onto)
+
+
 class Template(models.Model):
     tname = models.CharField(max_length=50, default='')
     # Owner to be constructed
     #owner = models.ForeignKey(User)
     description = models.TextField(blank=True)
     sample = models.TextField(blank=True)
-    data_src = models.CharField(max_length=150, default='')
+    # javascrip describe that template
+    js_src = models.CharField(max_length=150, default='')
     thumbnail_src = models.CharField(max_length=150, default='')
     # Store the struct of the template
     fields = models.ManyToManyField(Field, blank=True, null=True)
 
     def __unicode__(self):
         return '%d - %s' % (self.id, self.tname)
+
+
+class Graph(models.Model):
+    gname = models.CharField(max_length=50, default='')
+    # Owner to be constructed
+    #owner = models.ForeignKey(User)
+    description = models.TextField(blank=True)
+    # json file
+    data_src = models.CharField(max_length=150, default='')
+    mappings = models.ManyToManyField(Mapping, blank=True, null=True)
+
+    def __unicode__(self):
+        return '%d - %s' % (self.id, self.gname)
