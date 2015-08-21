6dbc5e3b10b064d3038dd4a8bab02023888a53c7
hydai <z54981220@gmail.com>
2015-08-22 05:00:19 +0800

    Finished graph view

--- graph/views.py
+++ graph/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from schema.models import Template
 
 
 def graph(request):
-    return render(request, 'graph.html')
+    templates = Template.objects.all()
+    return render(request, 'graph.html', {'templates': templates})
