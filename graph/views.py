c0bd7be22b8851068943fc33b94ae57f59d68997
henryyang42 <henryyang42@gmail.com>
2015-08-22 11:04:39 +0800

    fix bugs

--- graph/views.py
+++ graph/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from schema.models import Template, Mapping, Member
 from forms import CreateForm
-
+import json
+from django.http import JsonResponse
 
 def gallery(request):
     templates = Template.objects.all()
@@ -24,6 +25,15 @@ def create(request, tid):
                 mapping = Mapping.objects.create(field=field, onto=onto)
                 graph.mappings.add(mapping)
                 graph.save()
+
+            json_arr = json.loads(graph.data_src)
+            print json_arr
+            for i in range(len(json_arr)):
+                for mapping in graph.mappings.all():
+                    json_arr[i][mapping.field.fname] = json_arr[i][mapping.onto]
+                    del json_arr[i][mapping.onto]
+            print json_arr
+            return JsonResponse(json_arr, safe=False)
 
 
     return render(request, 'create_graph.html', {'form': form, 'fields': fields})
